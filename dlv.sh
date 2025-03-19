9ad98b4f9636a7dd84720c1162c8d06d1e6339e1
j-panzer <panzer@sub.uni-goettingen.de>
2025-03-19 16:10:22 +0100

    Update go-iiif config and startup script for debugging

--- dlv.sh
+++ dlv.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-##(cd /go-iiif/bin && dlv --listen=:40000 --headless=true exec ./iiif-server -- -host 0.0.0.0 -config-source file:///etc/go-iiif)
-dlv --listen=:2345 --headless=true --log=true --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc --accept-multiclient --api-version=2 exec /go/iiif-server -- -host 0.0.0.0 -config-source file:///etc/iiif-server/
-##(cd /bin && dlv --listen=:40000 --headless=true exec ./iiif-server -- -host 0.0.0.0 -config-source file:///etc/go-iiif)
+(cd /go &&          dlv  --listen=:2345 --headless=true --log=true --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc --accept-multiclient --api-version=2 exec ./iiif-server -- -server-uri http://0.0.0.0:8080 -config-source file:///etc/iiif-server)
